4ec17fdc2b46f4fcb24eda3931743b7dff67bd70
yuriko <yuriko40223@gmail.com>
2024-10-27 11:19:44 +0900

    ビュー修正1

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -7,70 +7,78 @@
 @section('content')
 
 <div class="cake__content">
-    <div class="cake__content-heading">
+    <div class="cake__heading">
         <h2>クリスマスケーキご予約フォーム</h2>
     </div>
-    <form action="/cakes/confirm" class="cake-form" method="post">
+    <form action="/cakes/confirm" class="form" method="post">
         @csrf
         
-        <div class="cake-form__group">
-            <div class="cake-form__heading">
-                <span class="cake-form__label">お名前</span>
-                <span class="cake-form__required">必須</span>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お名前</span>
+                <span class="form__label--required">必須</span>
             </div>
-            <div class="cake-form__item">
-                <input class="cake-form__item-input" type="text" name="name" placeholder="てすとアリス"value="{{ old('name') }}">
+            <div class="form__group-content">
+            <div class="form__input--text">
+                <input type="text" name="name" placeholder="てすとアリス"value="{{ old('name') }}">
             </div>
             <div class="form__error">
                 @error('name')
                 {{ $message }}
                 @enderror
             </div>
+            </div>
         </div>
- <div class="cake-form__group">
-            <div class="cake-form__heading">
-                <span class="cake-form__label">メールアドレス</span>
-                <span class="cake-form__required">必須</span>
+ <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">メールアドレス</span>
+                <span class="form__label--required">必須</span>
             </div>
-            <div class="cake-form__item">
-                <input class="cake-form__item-input" type="text" name="email" placeholder="test@example.com "value="{{ old('email') }}">
+            <div class="form__group-content">
+            <div class="form__input--text">
+                <input  type="text" name="email" placeholder="test@example.com "value="{{ old('email') }}">
             </div>
-            <div class="form-error">
+            <div class="form__error">
                 @error('email')
                 {{ $message }}
                 @enderror
             </div>
+            </div>
         </div>
-         <div class="cake-form__group">
-            <div class="cake-form__heading">
-                <span class="cake-form__label">電話番号</span>
-                <span class="cake-form__required">必須</span>
+         <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">電話番号</span>
+                <span class="form__label--required">必須</span>
             </div>
-            <div class="cake-form__item">
-                <input class="cake-form__item-input"type="text" name="tel" placeholder="0123456789"value="{{ old('tel') }}">
+            <div class="form__group-content">
+            <div class="form__input--text">
+                <input type="text" name="tel" placeholder="0123456789"value="{{ old('tel') }}">
             </div>
-            <div class="form-error">
+            <div class="form__error">
                 @error('tel')
                 {{ $message }}
                 @enderror  
             </div>
+            </div>
         </div>
-         <div class="cake-form__group">
-            <div class="cake-form__heading">
-                <span class="cake-form__label">ケーキ番号</span>
-                <span class="cake-form__required">必須</span>
+         <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">ケーキ番号</span>
+                <span class="form__label--required">必須</span>
             </div>
-            <div class="cake-form__item">
-                <input class="cake-form__item-input"type="text" name="number" placeholder="1"value="{{ old('number') }}">
+            <div class="form__group-content">
+            <div class="form__input--text">
+                <input type="text" name="number" placeholder="1"value="{{ old('number') }}">
             </div>
-            <div class="form-error">
+            <div class="form__error">
                 @error('number')
                 {{ $message }}
                 @enderror  
             </div>
+            </div>
         </div>
-        <div class="cake-form__button">
-            <button class="cake-form__button-subnit" type=""submit>予約</button>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">予約</button>
         </div>
     </form>
 </div>
